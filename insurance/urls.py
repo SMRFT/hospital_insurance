28ad021b169278ee86f0ade287aed03b67b92824
SMRFT <parthibansmrft@gmail.com>
2025-04-03 13:57:55 +0530

    Initial commit for backend

---
+++ insurance/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('registration/', views.registration, name='registration'),
+    path('login/', views.login, name='login'),
+    path('insurance/', views.insurance, name="Insurance"),
+    path('insurance/serve_file/<str:file_id>/', views.serve_file, name="serve_file"),  # Add this line for file serving
+]
