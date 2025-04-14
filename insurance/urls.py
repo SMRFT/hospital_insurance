fca6ea7fbc5a9dc926866680b974954d354923a7
SMRFT <110091550+SMRFT@users.noreply.github.com>
2025-04-14 11:29:39 +0530

    Update urls.py

--- insurance/urls.py
+++ insurance/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('insurance/', views.insurance, name="Insurance"),
     path('insurance/serve_file/<str:file_id>/', views.serve_file, name="serve_file"),  # Add this line for file serving
     path('submit-daycare/', views.submit_daycare, name='submit_daycare'),
-    path('insurance/update/<str:bill_number>/', views.insurance_update, name='insurance_update'),
+    path('insurance/update/<str:bill_number>/', views.insurance_update_combined, name='insurance_update'),
+    path('insurance/update_pendingamount/<str:bill_number>/', views.insurance_update_combined, name='insurance_update'),
 ]
