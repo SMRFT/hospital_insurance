a7710e4784f21f509fc4caa301cde4484f567364
SMRFT <110091550+SMRFT@users.noreply.github.com>
2025-10-16 11:20:31 +0530

    Update settings-prod.py

--- hospital_insurance/settings-prod.py
+++ hospital_insurance/settings-prod.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d^)yc)$9xs@yq$__z$0up-d4c%yykmrzps73v&0l=680wy3b6)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS= ["shinova.in"]
+ALLOWED_HOSTS= ["shinova.in",'https://shinova.in']
 CORS_ALLOW_CREDENTIALS = True
 
 
@@ -144,4 +144,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
